caa5312bc99bb338b6616fab012fdd3d3bf62002
arifanf <79965177+arifanf@users.noreply.github.com>
2022-01-21 08:56:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ st.write("""
 # Customer Payment Prediction
 
 This page to predict **Customer Payment** by Credition.""")
-st.write("Developed by Project team 2 : [Banyu](https://github.com/Kemi41), [Prissy](https://github.com/prissyyy), [Arifa](https://github.com/arifanf), [Mellisa](https://github.com/mellisairawan), [Taufiq](https://github.com/TaufiqHidayatullah528).
-")
+st.write("""Developed by Project team 2 : [Banyu](https://github.com/Kemi41), [Prissy](https://github.com/prissyyy), [Arifa](https://github.com/arifanf), [Mellisa](https://github.com/mellisairawan), [Taufiq](https://github.com/TaufiqHidayatullah528).
+""")
 
 st.sidebar.header('Input Data')
 
